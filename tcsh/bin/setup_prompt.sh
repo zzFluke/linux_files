cd51fc64b28820ba1eb6e6ea244a84e79f1eb525
Eric Chang <pkerichang@berkeley.edu>
2016-12-22 12:07:41 -0800

    adding csh setup files.

---
+++ tcsh/bin/setup_prompt.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash -f
+# bash script that prints out custom prompt banner
+
+# Grab host name
+hostnam=$(echo -n $HOSTNAME | sed -e "s/[\.].*//")
+# grab username
+usernam=$(whoami)
+# get current path
+newPWD="${PWD}"
+# compute prompt size
+promptsize=$(echo -n "--(${usernam}@${hostnam})---(${PWD})--" | wc -c | tr -d " ")
+
+# add extra dashes to fill terminal
+let fillsize=$(tput cols)-${promptsize}
+fill=""
+while [ "$fillsize" -gt "0" ] 
+do 
+   fill="${fill}-"
+   let fillsize=${fillsize}-1
+done
+
+# reduct path size
+if [ "$fillsize" -lt "0" ]
+then
+    let cut=3-${fillsize}
+    newPWD=$(echo -n $PWD | sed -e "s/\(^.\{$cut\}\)\(.*\)/\2/")
+fi
+
+# define colors
+NO_COLOUR="\033[0m"
+LIGHT_BLUE="\033[1;34m"
+YELLOW="\033[1;33m"
+
+# echo out banner
+echo -e "${YELLOW}-${LIGHT_BLUE}-(${YELLOW}${usernam}${LIGHT_BLUE}@${YELLOW}${hostnam}${LIGHT_BLUE})-\
+${YELLOW}-${fill}${LIGHT_BLUE}-(${YELLOW}${newPWD}${LIGHT_BLUE})-${YELLOW}-${NO_COLOR}"
